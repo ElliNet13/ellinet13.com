d694d092f58a6391dd267d21f6a038a1941eb0f7
ElliNet13 <elliotmoose4@gmail.com>
2025-07-28 10:37:54 -0600

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   output: 'server',       // Enable SSR build
   adapter: vercel(),      // Use Vercel Server adapter
-  integrations: [react()]
+  integrations: [react(), sitemap()]
 });
