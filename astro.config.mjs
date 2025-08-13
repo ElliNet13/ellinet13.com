a8f135946c585a3e3b2daf5ef309cf46ebdf98fd
ElliNet13 <elliotmoose4@gmail.com>
2025-08-12 19:10:46 -0600

    Upgrade astro and make WIP Puter powered v0 like AI and more

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
-
 import sitemap from '@astrojs/sitemap';
+import path from 'path';
 
 export default defineConfig({
   site: 'https://ellinet13.com',
   output: 'server',       // Enable SSR build
   adapter: vercel(),      // Use Vercel Server adapter
-  integrations: [react(), sitemap()]
+  integrations: [react(), sitemap()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src'),
+      },
+    },
+  },
 });
