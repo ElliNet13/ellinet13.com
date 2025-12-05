21b7829cabf392d497744a148c8c224f850cb53c
ElliNet13 <elliotmoose4@gmail.com>
2025-12-05 13:09:08 -0700

    Try to fix my service worker

--- public/sw.js
+++ public/sw.js
@@ -1,5 +1,37 @@
+// Install event
+self.addEventListener('install', event => {
+  console.log('Service Worker installing...');
+  self.skipWaiting(); // Activate immediately
+});
+
+// Activate event
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated');
+
+  // Thank the user for enabling push notifications
+  event.waitUntil(
+    self.registration.showNotification('Thanks!', {
+      body: 'Thanks for allowing push notifications!',
+      icon: '/logo.png'
+    })
+  );
+
+  // Claim clients immediately
+  event.waitUntil(self.clients.claim());
+});
+
+// Push event
 self.addEventListener('push', event => {
-  const data = event.data.json();
+  let data = { title: '[No title, this is a error]', body: '[No body, this is a error, please email ellinet13@ellinet13.com]' };
+
+  try {
+    if (event.data) {
+      data = event.data.json();
+    }
+  } catch (err) {
+    console.error('Push event data parse error', err);
+  }
+
   event.waitUntil(
     self.registration.showNotification(data.title, {
       body: data.body,
